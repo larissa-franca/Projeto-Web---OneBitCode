425f5f57c69fc64e56067b3da3b895b77f71abe0
larissaRF <larissa_lrf@hotmail.com>
2024-01-17 20:52:16 -0300

    inde

---
+++ Conceitos_.js
@@ -0,0 +1,4 @@
+//Linguagem de programação: JavaScript - cria funcionalidades
+//Linguagem de marcação: HTML - ele é o esqueleto da página web, é quem define toda a estrutura da página
+//Linguagem de estilização: CSS - parte visual, dar estilos e visual do site, cor, tamanho
+//Index: é entendido como pagina incial o indice do projeto
